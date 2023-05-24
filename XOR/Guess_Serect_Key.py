ef14e4544f4a0203554b0c223dad12bc80c42177
sitingwu <rosewu0901@gmail.com>
2023-05-24 12:49:03 +0200

    matrix2bytes

--- XOR/Guess_Serect_Key.py
+++ XOR/Guess_Serect_Key.py
@@ -97,4 +97,9 @@ for i in range(int(len(hex)/8)):
         print(i)
         
 print(flag)
-    
+
+#------------another faster answer---------------------------------------------- 
+from pwn import xor
+flag = bytes.fromhex('0e0b213f26041e480b26217f27342e175d0e070a3c5b103e2526217f27342e175d0e077e263451150104')
+print(xor(flag, 'crypto{'.encode())) # oh, it says 'myXORke+y...'
+print(xor(flag, 'myXORkey'.encode())) # try this? yay, it works! sometimes simpler is better
